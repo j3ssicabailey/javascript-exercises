404999d47cc26404b44bdab5d2853611c21d01f3
Jessica Bailey <j3ssicabailey@gmail.com>
2025-03-11 15:56:28 +0100

    Adding moving blue box exercise

---
+++ js/exercises-events-js/moving_blue_box_5.js
@@ -0,0 +1,22 @@
+// 5. Crea una rutina que no permita que el ratón se coloque encima de una caja azul de 100 x 100 px. Cada vez que el ratón intente colocarse encima, la posición de la caja debe cambiar aleatoriamente por la página.
+
+let blueBox = document.getElementById("avoidBox");
+
+let moveBox = () => {
+    blueBox.style.position = "absolute";
+    let maxWidth = window.innerWidth - blueBox.offsetWidth; // Prevent overflow
+    let maxHeight = window.innerHeight - blueBox.offsetHeight; 
+
+    let randomX = Math.floor(Math.random() * maxWidth);
+    let randomY = Math.floor(Math.random() * maxHeight);
+
+    blueBox.style.left = `${randomX}px`;
+    blueBox.style.top = `${randomY}px`;
+};
+
+
+blueBox.addEventListener("mouseover", moveBox);
+
+
+
+
