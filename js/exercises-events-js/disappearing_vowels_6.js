c8dda0ae3fc28533eb7c4bf5081a407d2e77ef23
Jessica Bailey <j3ssicabailey@gmail.com>
2025-03-11 16:19:59 +0100

    Adding exercise 6 on disappearing vowels

---
+++ js/exercises-events-js/disappearing_vowels_6.js
@@ -0,0 +1,16 @@
+// 6. Crea una aplicación que contenga un párrafo con mucho texto. Debajo del párrafo debe aparecer un botón por cada vocal. Al pulsar sobre una vocal, esa vocal desaparecerá del texto.
+
+let text = document.getElementById("vowelText");
+let vowelButtons = document.getElementById("vowelButtons");
+
+vowelButtons.addEventListener("click", (e) => {
+    let vowel = e.target.getAttribute("data-vowel");
+    let newText = text.textContent.split("").filter(letter =>
+        letter.toLowerCase() !== vowel).join("");
+    text.textContent = newText;
+});
+    
+    
+
+
+
